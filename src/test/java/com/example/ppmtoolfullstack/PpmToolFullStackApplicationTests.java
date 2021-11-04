3d0962e8da9a5691f3a98bd0d0c08d8f38aba2bd
Thuydd <thuydd91@gmail.com>
2021-11-04 21:41:28 +0700

    first commit

---
+++ src/test/java/com/example/ppmtoolfullstack/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppmtoolfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
